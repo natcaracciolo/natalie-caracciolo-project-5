18053a52a3db404c7ab3fa6a84c16ef5bbdaedf0
Natalie Caracciolo <nataliecaracciolo@gmail.com>
2019-05-31 20:28:16 -0400

    added particles background

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Titles from './Titles.js';
 import Results from './Results.js';
 import Particles from 'react-particles-js';
 
+
 class App extends Component {
   constructor() {
     super();
@@ -73,7 +74,57 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App wrapper">
+      <div className="App" id="particles">
+
+      <Particles 
+          params={{
+            "particles": {
+              "number": {
+                "value": 60,
+                "density": {
+                  "enable": true,
+                  "value_area": 1500
+                }
+              },
+              "line_linked": {
+                "enable": true,
+                "opacity": 0.02
+              },
+              "move": {
+                "direction": "right",
+                "speed": 0.05
+              },
+              "size": {
+                "value": 1
+              },
+              "opacity": {
+                "anim": {
+                  "enable": true,
+                  "speed": 1,
+                  "opacity_min": 0.05
+                }
+              }
+            },
+            "interactivity": {
+              "events": {
+                "onclick": {
+                  "enable": true,
+                  "mode": "push"
+                }
+              },
+              "modes": {
+                "push": {
+                  "particles_nb": 1
+                }
+              }
+            },
+            "retina_detect": true
+          }}
+      
+      
+      
+      
+      />
 
          <Titles /> 
 
--- src/Titles.js
+++ src/Titles.js
@@ -9,10 +9,6 @@ class Titles extends Component {
                 <h1>Daily Horoscope</h1>
                 <p>enter your astrological sign below to get some advice on your day! </p>
                 <p> not sure what your sign is? no problem. <a href="https://whatismysign.net/">click here </a>to find out. </p>
-
-              
-
-             
             </div>
 
             
