0af7723c8bb953fadb31048ec0ab350fc09298a5
Natalie Caracciolo <nataliecaracciolo@gmail.com>
2019-06-03 11:09:56 -0400

    Updates

---
+++ precache-manifest.994229dea4110763141f17f7c95308e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5627c57e2f554468db4d0f956fada605",
+    "url": "/natalie-caracciolo-project-5/index.html"
+  },
+  {
+    "revision": "01e5e2503c5066de232b",
+    "url": "/natalie-caracciolo-project-5/static/css/main.9b70e1d8.chunk.css"
+  },
+  {
+    "revision": "e05ac8627261dcb2d9e7",
+    "url": "/natalie-caracciolo-project-5/static/js/2.870e5b0a.chunk.js"
+  },
+  {
+    "revision": "01e5e2503c5066de232b",
+    "url": "/natalie-caracciolo-project-5/static/js/main.93ca7a8a.chunk.js"
+  },
+  {
+    "revision": "e6baee02977f56bb92eb",
+    "url": "/natalie-caracciolo-project-5/static/js/runtime~main.b90c8c30.js"
+  }
+]);
